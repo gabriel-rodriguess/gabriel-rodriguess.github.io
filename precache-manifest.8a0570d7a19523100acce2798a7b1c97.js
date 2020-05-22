4be05f1a04215cb9a8a2e70579cfe92170312d52
Gabriel <rodriguesgg36@gmail.com>
2020-05-21 22:25:47 -0300

    Updates

---
+++ precache-manifest.8a0570d7a19523100acce2798a7b1c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2914a6dabbff786a7d9d41c303f92a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84928d4b0209bad5028f",
+    "url": "/static/css/main.3992a018.chunk.css"
+  },
+  {
+    "revision": "2124662fc84c7c87af25",
+    "url": "/static/js/2.82c24fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82c24fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84928d4b0209bad5028f",
+    "url": "/static/js/main.41ee0d10.chunk.js"
+  },
+  {
+    "revision": "b3492579928b43eb27ac",
+    "url": "/static/js/runtime-main.869c1cb8.js"
+  },
+  {
+    "revision": "ce93820d828ba3006575cffa6e242c17",
+    "url": "/static/media/program_wallpaper_5blur.ce93820d.png"
+  }
+]);
